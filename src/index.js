9a6f6d086ca1976f3db75816b9ad73f120debdd6
Kelly Selden <kellyselden@gmail.com>
2022-12-03 12:11:49 -0800

    implement

---
+++ src/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function reporter(context, {
+  newline = 'always'
+}) {
+  let {
+    Syntax,
+    getSource,
+    report,
+    RuleError,
+    fixer
+  } = context;
+
+  return {
+    [Syntax.Document](node) {
+      let text = getSource(node);
+
+      let match = text.match(/\r?\n$/m);
+
+      if (newline === 'always' && !match) {
+        report(node, new RuleError('Newline required at end of file but not found.', {
+          index: text.length,
+          fix: fixer.replaceTextRange([text.length, text.length], `
+`)
+        }));
+      }
+
+      if (newline === 'never' && match) {
+        report(node, new RuleError('Newline not allowed at end of file.', {
+          index: match.index,
+          fix: fixer.replaceTextRange([match.index, match.index + match[0].length], '')
+        }));
+      }
+    }
+  };
+}
+
+module.exports = {
+  linter: reporter,
+  fixer: reporter
+};
