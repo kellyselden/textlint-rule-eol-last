3dd96feab5152501b542c47e9647ed5695f958b6
Kelly Selden <kellyselden@gmail.com>
2022-12-03 12:11:38 -0800

    ember-cli-update init -b @kellyselden/node-template --repo-slug=kellyselden/textlint-rule-eol-last --github-actions --renovate

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  env: {
+    es6: true
+  },
+  extends: [
+    'sane-node'
+  ],
+  overrides: [
+    {
+      files: [
+        'test/**/*-test.js'
+      ],
+      env: {
+        mocha: true
+      },
+      plugins: [
+        'mocha'
+      ],
+      extends: [
+        'plugin:mocha/recommended'
+      ],
+      rules: {
+        'mocha/no-exclusive-tests': 'error',
+        'mocha/no-empty-description': 'off'
+      }
+    }
+  ]
+};
